5e6c8db191a7ad28bce18255600a33b8a3ba58f9
parisa <parisa.naeim@gmail.com>
2023-06-22 23:03:03 +1000

    add CreateTask component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import CreateTask from "./components/CreateTask";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="container mt-4 mb-4">
+        <div className="row">
+          <div className="col-md-4 col-sm-6">
+            <div className="user-form p-4 border rounded-2" id="create-form">
+              <CreateTask />
+            </div>
+          </div>
+        </div>
+      </div>
       <Footer />
     </div>
   );
---
+++ src/components/CreateTask.js
@@ -0,0 +1,67 @@
+function CreateTask() {
+  return (
+    <div>
+      <label for="name" className="form-label">
+        Name
+      </label>
+      <input type="text" className="form-control" id="name" name="name" />
+      <p id="invalidName" className="error"></p>
+      <br />
+      <label for="description" className="form-label">
+        Description
+      </label>
+      <textarea
+        className="form-control"
+        id="description"
+        name="description"
+      ></textarea>
+      <p id="invalidDescription" className="error"></p>
+      <br />
+      <label for="assigned-to" className="form-label">
+        Assigned to
+      </label>
+      <input
+        type="text"
+        className="form-control"
+        id="assigned-to"
+        name="assigned-to"
+      />
+      <p id="invalidAssignedTo" className="error"></p>
+      <br />
+      <label for="due-Date" className="form-label">
+        Due Date
+      </label>
+      <input
+        type="date"
+        className="form-control"
+        id="due-Date"
+        name="due-Date"
+      />
+      <br />
+      <label for="status" className="form-label">
+        Status
+      </label>
+      <select name="status" className="form-select" id="status">
+        <option value="to-do">To Do</option>
+        <option value="in-progress">In Progress</option>
+        <option value="review">Review</option>
+        <option value="done">Done</option>
+      </select>
+      <br />
+      <input
+        type="submit"
+        className="btn btn-primary"
+        value="Create"
+        id="create"
+      />
+      <input
+        type="reset"
+        className="btn btn-secondary"
+        value="Reset"
+        id="reset"
+      />
+    </div>
+  );
+}
+
+export default CreateTask;
