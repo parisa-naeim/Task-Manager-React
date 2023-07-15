62f300dfb3b96b6209e3f1732eccb6ebd8833f2a
parisa <parisa.naeim@gmail.com>
2023-07-15 22:37:03 +1000

    improve style

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,45 +2,45 @@ import CardAvatar from "./CardAvatar";
 
 function Task({ task }) {
   return (
-    <div class="col-md-6 col-lg-4 col-12" id={task.id + "-original"}>
-      <div class="card">
+    <div className="col-md-6 col-lg-4 col-12" id={task.id + "-original"}>
+      <div className="card">
         <CardAvatar assignedTo={task.assignedTo} />
-        <div class="card-body">
-          <h5 class="card-title">{task.name}</h5>
-          <h6 class="card-subtitle mb-2 text-body-secondary">
+        <div className="card-body">
+          <h5 className="card-title">{task.name}</h5>
+          <h6 className="card-subtitle mb-2 text-body-secondary">
             {task.status && task.status.toUpperCase()}
           </h6>
-          <p class="card-text"> {task.description}</p>
+          <p className="card-text"> {task.description}</p>
           {/* <label>{task.assignedTo}</label>
           <br /> */}
           <label>{task.dueDate}</label>
           <br />
           <br />
-          <span class="material-symbols-outlined delete-btn" id={task.id}>
+          <span className="material-symbols-outlined delete-btn" id={task.id}>
             delete
           </span>
-          <span class="material-symbols-outlined edit-btn" id={task.id}>
+          <span className="material-symbols-outlined edit-btn" id={task.id}>
             stylus
           </span>
-          {/* <span style="display:${(task.status === "done") ? "none" : "inilne-block"}" class="material-symbols-outlined mark-as-done" title="Mark as done" id="${task.id}">done</span> */}
+          {/* <span style="display:${(task.status === "done") ? "none" : "inilne-block"}" className="material-symbols-outlined mark-as-done" title="Mark as done" id="${task.id}">done</span> */}
         </div>
       </div>
 
       <div
-        class="col-md-6 col-lg-4 col-12"
+        className="col-md-6 col-lg-4 col-12"
         id={task.id + "-editable"}
         style={{ display: "none" }}
       >
-        <div class="card pt-3">
-          <div class="change">
-            <span class="material-symbols-outlined save-btn" id={task.id}>
+        <div className="card pt-3">
+          <div className="change">
+            <span className="material-symbols-outlined save-btn" id={task.id}>
               save
             </span>
-            <span class="material-symbols-outlined cancel-btn" id={task.id}>
+            <span className="material-symbols-outlined cancel-btn" id={task.id}>
               cancel
             </span>
           </div>
-          <div class="card-body">
+          <div className="card-body">
             <input id={task.id + "-name"} value={task.name} type="text" />
             <input id={task.id + "-status"} value={task.status} />
             <input
